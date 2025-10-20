d1b72057bbb7a6bd2cf05a92bc4750232d0ecf6a
Sidharth Saha <ssaha2000@gmail.com>
2025-10-20 11:49:29 -0700

    Create fft_2c2 and fft_r2c.

---
+++ include/fft_accelerator.h
@@ -0,0 +1,10 @@
+#include <stddef.h>
+
+typedef struct
+{
+	double real;
+	double imaginary;
+} fft_double_complex;
+
+int fft_c2c(const fft_double_complex* input, fft_double_complex* output, size_t size);
+int fft_r2c(const double* input, fft_double_complex* output, size_t size);
---
+++ src/fft_accelerator.c
@@ -0,0 +1,13 @@
+#include "fft_accelerator.h"
+
+int fft_c2c(const fft_double_complex* input, fft_double_complex* output, size_t size)
+{
+	// TODO implementation
+	return 0;
+}
+
+int fft_r2c(const double* input, fft_double_complex* output, size_t size)
+{
+	// TODO implementation
+	return 0;
+}
